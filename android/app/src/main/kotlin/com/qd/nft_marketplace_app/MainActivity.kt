e4fa5a5a3472477cd47ed08be794e3a0d210cec9
QD1221 <zxcvbnm12122000@gmail.com>
2021-09-24 11:27:51 +0700

    Initial commit

---
+++ android/app/src/main/kotlin/com/qd/nft_marketplace_app/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.qd.nft_marketplace_app
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/ui/profile_page.dart
@@ -0,0 +1,124 @@
+import 'package:flutter/material.dart';
+
+class ProfilePage extends StatefulWidget {
+  const ProfilePage({Key? key}) : super(key: key);
+
+  @override
+  _ProfilePageState createState() => _ProfilePageState();
+}
+
+class _ProfilePageState extends State<ProfilePage> {
+  @override
+  Widget build(BuildContext context) {
+    return DefaultTabController(
+      length: 2,
+      initialIndex: 0,
+      child: Scaffold(
+        body: SafeArea(
+          child: Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: Column(
+              children: [
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    IconButton(
+                      onPressed: () {},
+                      icon: Icon(Icons.keyboard_arrow_left),
+                    ),
+                    IconButton(
+                      onPressed: () {},
+                      icon: Icon(Icons.more_vert),
+                    )
+                  ],
+                ),
+                CircleAvatar(
+                  radius: 44,
+                  backgroundImage: NetworkImage(
+                      'https://cdn.pixabay.com/photo/2016/01/25/06/12/beijing-opera-1160109_960_720.jpg'),
+                ),
+                SizedBox(
+                  height: 16,
+                ),
+                Text(
+                  'Hello World',
+                  style: TextStyle(fontWeight: FontWeight.bold),
+                ),
+                TextButton(
+                  onPressed: () {},
+                  child: Text('@unknown'),
+                  style: TextButton.styleFrom(
+                    padding: EdgeInsets.zero,
+                    minimumSize: Size(0, 0),
+                  ),
+                ),
+                Padding(
+                  padding: const EdgeInsets.symmetric(horizontal: 16),
+                  child: Text(
+                    'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,'
+                    ' quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.',
+                    textAlign: TextAlign.center,
+                    style: TextStyle(
+                        fontWeight: FontWeight.normal, color: Colors.grey),
+                  ),
+                ),
+                Padding(
+                  padding: const EdgeInsets.symmetric(vertical: 16),
+                  child: Text(
+                    'hello@world.com',
+                    style: TextStyle(fontWeight: FontWeight.bold),
+                  ),
+                ),
+                TabBar(
+                  tabs: [
+                    Tab(text: 'Creations'),
+                    Tab(text: 'Collections'),
+                  ],
+                  labelColor: Colors.black,
+                  unselectedLabelColor: Colors.grey,
+                  indicatorColor: Colors.black,
+                  indicatorWeight: 1,
+                ),
+                Expanded(
+                  child: TabBarView(children: [
+                    Padding(
+                      padding: const EdgeInsets.only(top: 16),
+                      child: GridView.count(
+                        crossAxisCount: 2,
+                        children: List.generate(
+                          8,
+                          (index) => Container(
+                            decoration: BoxDecoration(
+                              borderRadius: BorderRadius.circular(8),
+                              color: Colors.pink,
+                            ),
+                          ),
+                        ),
+                        mainAxisSpacing: 8,
+                        crossAxisSpacing: 8,
+                      ),
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.only(top: 16),
+                      child: GridView.count(
+                        crossAxisCount: 2,
+                        children: List.generate(
+                          8,
+                          (index) => Container(
+                            color: Colors.red,
+                          ),
+                        ),
+                        mainAxisSpacing: 8,
+                        crossAxisSpacing: 8,
+                      ),
+                    ),
+                  ]),
+                ),
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
